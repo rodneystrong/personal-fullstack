1884bc3b48898e7635c23ef4fddef3b54446afc1
aleeex <akang2@gmail.com>
2016-08-29 04:27:51 -0600

    required massiveJS +connected to DB

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var bodyParser = require('body-parser');
 
 var app = module.exports = express();
 
+var connectionString = "postgress://aleeexkang@localhost/personal_fullstack"; // "postgess://username/host/databaseName"
+var massiveInstance = massive.connectSync({connectionString : connectionString}); // who knows
+app.set("db", massiveInstance); // setting the "db" obj to equal th massive.connectSync
+
 app.listen(3030, function() {
   console.log('lel');
 });
